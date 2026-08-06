d0f0f9d136306918187cc1ef54c6c04d3706fc21
whatuknow <csong1501@gmail.com>
2026-08-06 15:15:20 +0900

    파이썬 공부

---
+++ 02_PYTHON/main.py
@@ -0,0 +1,9 @@
+def main():
+    print("Hello from first-project!")
+
+
+if __name__ == "__main__":
+    main()
+
+print("name: 프로젝트의 이름을 지정하는 메타데이터 항목입니다.")
+print("requires-python: 프로젝트에서 필요한 최소 Python 버전을 지정하는 메타데이터 항목입니다.")
